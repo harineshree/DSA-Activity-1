3979848739f834572c759104f364d6d74ec62faf
harineshree <harineshree483@gmail.com>
2026-04-08 22:30:02 +0530

    Implement max subarray function in C

---
+++ max_subarray.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int maxSubArray(int nums[], int size) {
+    int maxSum = nums[0];
+    int currentSum = nums[0];
+
+    for(int i = 1; i < size; i++) {
+        if(currentSum < 0)
+            currentSum = nums[i];
+        else
+            currentSum += nums[i];
+
+        if(currentSum > maxSum)
+            maxSum = currentSum;
+    }
+
+    return maxSum;
+}
+
+int main() {
+    int nums[] = {-2,1,-3,4,-1,2,1,-5,4};
+    int size = 9;
+
+    printf("Output: %d", maxSubArray(nums, size));
+    return 0;
+}
